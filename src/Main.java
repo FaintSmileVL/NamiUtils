4477f76cfd8e2ad919deb2d3fd53d3528ca23461
Nami <faintsmilevl@gmail.com>
2023-05-14 13:06:16 +0600

    refactor 2

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,7 @@
 import makers.BroadcastPacketMaker;
 import makers.SendPacketMaker;
 
-import java.io.*;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.io.File;
 
 /**
  * @author ${USER}
@@ -37,13 +27,11 @@ public class Main {
         //String innerPath = "\\src";
         String innerPath = "\\gameserver";
         path = path + innerPath;
-        File packetSenders = new File(path.toString());
-        File packetSenders2 = new File(path.toString());
-        File packetSenders3 = new File(path.toString());
+        String exceptionsPath = "\\result\\EXCEPTIONS";
 
-        SendPacketMaker.getInstance().movePacketSendersToNewStandard(packetSenders, "\\result\\EXCEPTIONS", innerPath);
-        BroadcastPacketMaker.getInstance().movePacketBroadcastersToNewStandard(packetSenders2, "\\result\\EXCEPTIONS", innerPath);
-        BroadcastPacketMaker.getInstance().movePacketBroadcasters2ToNewStandard(packetSenders3, "\\result\\EXCEPTIONS", innerPath);
+        SendPacketMaker.getInstance().movePacketSendersToNewStandard(new File(path.toString()), exceptionsPath, innerPath);
+        BroadcastPacketMaker.getInstance().movePacketBroadcastersToNewStandard(new File(path.toString()), exceptionsPath, innerPath);
+        BroadcastPacketMaker.getInstance().movePacketBroadcasters2ToNewStandard(new File(path.toString()), exceptionsPath, innerPath);
     }
 
 
